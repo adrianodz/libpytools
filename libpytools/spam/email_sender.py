05a9bf15f2f16609253e2f7543c9d043a63e1a6c
adrianodz <adriano.castan@gmail.com>
2020-10-03 15:54:11 -0300

    Updating tests

--- libpytools/spam/email_sender.py
+++ libpytools/spam/email_sender.py
@@ -1,3 +1,9 @@
 class Sender:
     def send(self, from_name, to_name, subject, body):
+        if '@' not in from_name:
+            raise InvalidEmail(f'invalid sender email {from_name}')
         return from_name
+
+
+class InvalidEmail(Exception):
+    pass
--- libpytools/tests/test_spam/test_email_sender.py
+++ libpytools/tests/test_spam/test_email_sender.py
@@ -1,6 +1,6 @@
 import pytest
 
-from libpytools.spam.email_sender import Sender
+from libpytools.spam.email_sender import Sender, InvalidEmail
 
 
 def test_email_sender():
@@ -14,6 +14,7 @@ def test_email_sender():
 def test_sender(sender_field):
     sender = Sender()
     # sender_fields = ['email1@email.com', 'foo@bar.com']
+    # for sender_field in sender_fields
     result = sender.send(
         sender_field,
         'email2@email.com',
@@ -21,3 +22,18 @@ def test_sender(sender_field):
         'First time.',
     )
     assert sender_field in result
+
+
+@pytest.mark.parametrize(
+    'sender_field',
+    ['', 'foo']
+)
+def test_invalid_sender(sender_field):
+    sender = Sender()
+    with pytest.raises(InvalidEmail):
+        result = sender.send(
+            sender_field,
+            'email2@email.com',
+            'Python Course',
+            'First time.',
+        )
